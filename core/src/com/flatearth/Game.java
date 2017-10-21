fa7a9143fb01b13b0823b3f1badd9db13770764b
maudinski <mauriciomartinez0707@gmail.com>
2017-10-21 15:33:39 -0500

    test

--- core/src/com/flatearth/Game.java
+++ core/src/com/flatearth/Game.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Matrix4;
