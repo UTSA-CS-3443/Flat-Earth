c8a7dff5446235c07d45d852a75dc22b045c0da3
maudinski <mauriciomartinez0707@gmail.com>
2017-10-21 21:00:58 -0500

    bit field

--- core/src/controller/Logic.java
+++ core/src/controller/Logic.java
@@ -2,13 +2,14 @@ package controller;
 
 import utilities.KeyboardState;
 import utilities.GameState;
+import utilities.KeysPressed;
 import java.lang.Thread;
 
 /**
  * logic part of the server. Runs concurrently with ServerSender and ServerReceiver. Receives 
  * KeyboardState objects from client, updates the GameState, then sets that game state so the Sender
  * can send it.
- * TODO relationship between this and sender should be that sender waits for a singla from this to send
+ * TODO relationship between this and sender should be that sender waits for a single from this to send
  * 
  * @author mauricio
  *
@@ -21,6 +22,8 @@ public class Logic implements Runnable {
 	// updated and sent to client in Sender
 	private GameState gs;
 	
+	private static final int[][] DIRECTIONS = {{225, 270, 315},{180, -1, 0},{135, 90, 45}};
+	private static final int FORCECONSTANT = 50000;
 	//private static final int TIME_INTERVAL_MS = 10;
 	
 	public Logic(KeyboardState ks, GameState gs) {
@@ -30,87 +33,45 @@ public class Logic implements Runnable {
 	
 	@Override
 	public void run() {
-		boolean [] pressed;
+		KeysPressed pressed;
 		float direction = 0, forceX = 0, forceY = 0;
 		boolean movement;
 		
-		// some timestamping stuff recommended by Mark Robinson
-		// he's saying that synchronized methods have a solid amount of over hang to them. so
-		// it's better to slow down your process
-		//long currentTime = System.currentTimeMillis();
-		//long lastCallTime = 0;
-		
 		while(!Thread.currentThread().isInterrupted()) {
-			// time stamp stuff
-			/*currentTime = System.currentTimeMillis();
-			if (currentTime -lastCallTime < TIME_INTERVAL_MS) {
-				continue;
-			}
-			lastCallTime = currentTime;*/
 			// really gotta work on this threading stuff
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			// this synchronized method call is expensive as Mark Robins said (cause synchronized
-			// has a lot of over hang
 			pressed = this.ks.get();
-			//for (boolean b : pressed)
-			//	System.out.println(b);
-			// OPTIMIZE there's gotta be a cleaner way to do this
-			forceX = forceY = direction = 0;
-			// basically strict copy and paste from ash's code. no keys.UP though
-			// set forces and direction depending on what arrows are set
-			// pressed == [up, down, left, right]
-			if(pressed[0])
-			{
-				forceY = 50000;
-				direction = 90;
-				if(pressed[2])
-					direction = 45;
-				else if(pressed[3])
-					direction = 135;
-			}
-			else if(pressed[1])
-			{
-				forceY = -50000;
-				direction = 270;
-				if(pressed[2])
-					direction = 225;
-				else if(pressed[3])
-					direction = 315;
-			}
-			if(pressed[2])
-			{
-				forceX = -50000;
-				direction = 180;
-				if(pressed[0])
-					direction = 135;
-				else if(pressed[1])
-					direction = 225;
-			}
-			else if(pressed[3])
-			{
-				forceX = 50000;
-				direction = 0;
-				if(pressed[0])
-					direction = 45;
-				else if(pressed[1])
-					direction = 315;
-			}
-			if (forceX == 0 && forceY == 0) {
-				movement = false;
-			} else {
-				movement = true;
-			}
+			
+			// using boolean arithmetic, this would replace all that shit down there
+			forceX = (pressed.left * -FORCECONSTANT) + (pressed.right *FORCECONSTANT);
+			forceY = (pressed.up * FORCECONSTANT) + (pressed.down * -FORCECONSTANT);
+			direction = DIRECTIONS[(-pressed.down + pressed.up)+1][(-pressed.left+pressed.right)+1];
+			movement = !(forceX == 0 && forceY == 0);
 			// set the object
 			synchronized(this.gs) {
 				gs.setCharStats(movement, forceX, forceY, direction);
 			}
 		}
 	}
-
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ core/src/utilities/KeysPressed.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public class KeysPressed {
+	
+	public int up = 0;
+	public int down = 0;
+	public int right = 0;
+	public int left = 0;
+	
+	public int defend = 0;
+	public int attack1 = 0;
+	public int attack2 = 0;
+	
+}
