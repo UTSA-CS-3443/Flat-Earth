dfbe64327c3fd6708139735e63ce33b9780df32b
Diego <digwallace@gmail.com>
2017-11-11 14:48:04 -0600

    Work on Enemy AI

--- core/src/server/entities/ServerNpc.java
+++ core/src/server/entities/ServerNpc.java
@@ -6,13 +6,34 @@ import com.badlogic.gdx.physics.box2d.Body;
 import communicators.serverToClient.CharacterState;
 import utilities.Exit;
 
-public class ServerNpc extends ServerCharacter {
+public class ServerNpc extends ServerCharacter
+{
 
-	public ServerNpc(Body body) {
+	public ServerNpc(Body body)
+	{
 		super(body);
 	}
 
-	public void update(Vector2 vectors[]) {
+	public void update(Vector2 vectors[])
+	{
+
+		Vector2 closest;
+
+		for(Vector2 target: vectors)
+		{
+			if (Math.abs(vectors[target].x - this.body.getPosition().x) +
+					Math.abs(vectors[target].x - this.body.getPosition().y))
+			{
+				closest = target;
+			}
+		}
+
+		if (Math.sqrt(closest.x * closest.x + closest.y * closest.y) <= 30f)
+		{
+
+		}
+
+
 		// find closest player, go towards them
 		// if close enough to attack, start/keep attacking
 		// TODO 
@@ -20,7 +41,8 @@ public class ServerNpc extends ServerCharacter {
 	}
 	
 	@Override
-	public CharacterState getState() {
+	public CharacterState getState()
+	{
 		return new CharacterState(this.movement, this.body.getPosition().x, this.body.getPosition().y, this.direction);
 	}
 	
