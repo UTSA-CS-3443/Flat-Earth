fd423c8bf223aa771903b1fe12b1cc168b1c53e5
Diego <digwallace@gmail.com>
2017-12-07 22:45:48 -0600

    JavaFX debug branch

--- desktop/src/startGui/Host_Controller.java
+++ desktop/src/startGui/Host_Controller.java
@@ -1,4 +1,53 @@
 package startGui;
 
-public class Host_Controller {
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Host_Controller implements EventHandler{
+
+    @FXML
+    BorderPane back;
+
+    @FXML
+    Label num;
+
+    @Override
+    public void handle(Event e) {
+        Button pressed = (Button) e.getSource();
+        String name = pressed.getId();
+
+        if (name.equals("listen")) {
+            num.setText("Clients Connected : 2");
+        } else if (name.equals("host")) {
+            loadMultiPlayerScene();
+        }
+    }
+
+    private void loadMultiPlayerScene()
+    {
+        Parent singlePlayer;
+        try {
+            singlePlayer = (BorderPane) FXMLLoader.load(getClass().getResource("/View/MultiPlayer.fxml"));
+            Scene scene  = new Scene(singlePlayer, 900,900);
+            Stage currStage = (Stage) back.getScene().getWindow();
+            currStage.setScene(scene);
+        } catch (IOException e) {
+            Logger.getLogger(MainMenu_Controller.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 }
--- desktop/src/startGui/MultiPlayer_Controller.java
+++ desktop/src/startGui/MultiPlayer_Controller.java
@@ -1,17 +1,34 @@
 package startGui;
 
 import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MultiPlayer_Controller implements EventHandler {
 
     @FXML
     BorderPane background;
+
+    public void initialize() {
+        background.setOpacity(0);
+        fadeInTransition();
+    }
 
     private void fadeInTransition(){
         FadeTransition fade = new FadeTransition();
@@ -25,6 +42,69 @@ public class MultiPlayer_Controller implements EventHandler {
 
     @Override
     public void handle(Event event) {
+        Button pressed = (Button) event.getSource();
+        String name = pressed.getId();
 
+        if (name.equals("host")) {
+            loadHostScene();
+        } else if (name.equals("client")) {
+            //loadClientScene();
+        } else if ( name.equals("back") ){
+            loadMainScene();
+        }
+    }
+//
+//    private void loadClientScene() {
+//        Parent singlePlayer;
+//        try {
+//            singlePlayer = (BorderPane) FXMLLoader.load(getClass().getResource("/View/Client.fxml"));
+//            Scene scene  = new Scene(singlePlayer, 900,900);
+//            Stage currStage = (Stage) background.getScene().getWindow();
+//            currStage.setScene(scene);
+//        } catch (IOException e) {
+//            Logger.getLogger(MainMenu_Controller.class.getName()).log(Level.SEVERE, null, e);
+//        }
+//    }
+
+    private void loadMainScene()
+    {
+        Parent singlePlayer;
+        try {
+            singlePlayer = (GridPane) FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
+            Scene scene  = new Scene(singlePlayer, 900,900);
+            Stage currStage = (Stage) background.getScene().getWindow();
+            currStage.setScene(scene);
+        } catch (IOException e) {
+            Logger.getLogger(MainMenu_Controller.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    private void makeFadeOutHost()
+    {
+        FadeTransition fade = new FadeTransition();
+        fade.setDuration(Duration.seconds(1.0));
+        fade.setNode(background);
+        fade.setFromValue(1.0);
+        fade.setToValue(0);
+        fade.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                loadHostScene();
+            }
+        });
+        fade.play();
+    }
+
+    private void loadHostScene()
+    {
+        Parent singlePlayer;
+        try {
+            singlePlayer = (BorderPane) FXMLLoader.load(getClass().getResource("/View/Host.fxml"));
+            Scene scene  = new Scene(singlePlayer, 900,900);
+            Stage currStage = (Stage) background.getScene().getWindow();
+            currStage.setScene(scene);
+        } catch (IOException e) {
+            Logger.getLogger(MainMenu_Controller.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 }
